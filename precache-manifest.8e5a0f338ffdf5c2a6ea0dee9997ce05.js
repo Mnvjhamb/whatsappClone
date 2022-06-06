47f82b1558630cadfc052fa8796e4d7ec6e72b6d
Manav Jhamb <manavjhamb002@gmail.com>
2022-06-06 16:40:33 +0530

    Updates

---
+++ precache-manifest.8e5a0f338ffdf5c2a6ea0dee9997ce05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87c47433917f2c7acc267c109681a4e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "695fad104cfb6feeda43",
+    "url": "/my-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "695fad104cfb6feeda43",
+    "url": "/my-app/static/js/2.4c5e4d85.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/my-app/static/js/2.4c5e4d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168a3f5606ca3528cd09",
+    "url": "/my-app/static/js/main.c6a7e2d8.chunk.js"
+  },
+  {
+    "revision": "7b27bf594e4f003582bb",
+    "url": "/my-app/static/js/runtime-main.5ba1a8c7.js"
+  }
+]);
